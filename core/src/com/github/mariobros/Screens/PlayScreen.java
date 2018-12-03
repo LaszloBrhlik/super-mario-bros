d9ddb1a77c012f21649d692c1ac08571fe59e22d
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-12-03 22:26:01 +0100

    update PlayScreen: FitViewport

--- core/src/com/github/mariobros/Screens/PlayScreen.java
+++ core/src/com/github/mariobros/Screens/PlayScreen.java
@@ -3,17 +3,27 @@ package com.github.mariobros.Screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.github.mariobros.MarioBros;
 
 public class PlayScreen implements Screen {
 
   private MarioBros game;
   private Texture texture;
+  private OrthographicCamera gameCam;
+  private Viewport gamePort;
 
   public PlayScreen (MarioBros game) {
     this.game = game;
     this.texture = new Texture("badlogic.jpg");
+    gameCam = new OrthographicCamera();
+    gamePort = new FitViewport(800, 480, gameCam);
   }
 
   @Override
@@ -22,9 +32,10 @@ public class PlayScreen implements Screen {
   }
 
   @Override
-  public void render(float delta) {
+  public void render(float delta ) {
     Gdx.gl.glClearColor(1, 0, 0, 1);
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    game.batch.setProjectionMatrix(gameCam.combined);
     game.batch.begin();
     game.batch.draw(texture, 0, 0);
     game.batch.end();
@@ -32,7 +43,7 @@ public class PlayScreen implements Screen {
 
   @Override
   public void resize(int width, int height) {
-
+    gamePort.update(width, height);
   }
 
   @Override

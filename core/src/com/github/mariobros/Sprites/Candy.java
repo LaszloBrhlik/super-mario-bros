c20384301ad8a929fcbe644e171c4f60ca92c055
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-12-26 22:41:13 +0100

    feature: InteractiveTileObject onHeadHit

--- core/src/com/github/mariobros/Sprites/Candy.java
+++ core/src/com/github/mariobros/Sprites/Candy.java
@@ -4,11 +4,13 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.World;
+import com.github.mariobros.MarioBros;
 
 public class Candy extends InteractiveTileObject {
   public Candy(World world, TiledMap map, Rectangle bounds) {
     super(world, map, bounds);
     fixture.setUserData(this);
+    setCategoryFilter(MarioBros.CANDY_BIT);
   }
 
   @Override

d9c282c60da3f36451dbf15cb2bf59c565952adc
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-12-27 12:45:18 +0100

    feature: Hud addScore

--- core/src/com/github/mariobros/Sprites/Brick.java
+++ core/src/com/github/mariobros/Sprites/Brick.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.World;
 import com.github.mariobros.MarioBros;
+import com.github.mariobros.Scenes.Hud;
 
 public class Brick extends InteractiveTileObject {
   public Brick(World world, TiledMap map, Rectangle bounds) {
@@ -16,5 +17,6 @@ public class Brick extends InteractiveTileObject {
   public void onHeadHit() {
     setCategoryFilter(MarioBros.DESTROYED_BIT);
     getCell().setTile(null);
+    Hud.addScore(200);
   }
 }
